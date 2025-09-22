0923b9525117dc54d7fa2442df7b5bda3c32cbd4
Tim Morgner <tim@timmorgner.de>
2025-09-23 00:59:51 +0200

    Entferne die Anforderung, dass die SQLite-Datenbankdatei existieren muss, um die Flexibilität bei der Datenbankinitialisierung zu erhöhen.

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 import type { AdapterAccountType } from "next-auth/adapters";
 import Database from "better-sqlite3";
 
-const sqlite = new Database ('crewnow.db', { readonly: false, fileMustExist: true });
+const sqlite = new Database ('crewnow.db', { readonly: false });
 export const db = drizzle({client: sqlite,logger: true});
 
 export const users = sqliteTable("users", {
